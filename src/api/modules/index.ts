0bf4980fa216c0886c497ab6bce1db04a9c62ba8
JamesWuVip <156403378+JamesWuVip@users.noreply.github.com>
2025-08-29 03:39:41 -0700

    feat: 新增API模块统一导出文件

---
+++ src/api/modules/index.ts
@@ -0,0 +1,9 @@
+// API模块统一导出
+export { default as authApi } from './auth'
+export { default as courseApi } from './course'
+export { default as adminApi } from './admin'
+
+// 导出所有API服务
+export * from './auth'
+export * from './course'
+export * from './admin'
