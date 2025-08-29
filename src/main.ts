3b8d53c0fc772a5fc8be4b4f1735265bf0798ea1
JamesWuVip <156403378+JamesWuVip@users.noreply.github.com>
2025-08-28 21:41:37 -0700

    feat: 添加应用入口文件main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import './style.css'
 import App from './App.vue'
 import router from './router'
+import pinia from './store'
 
+// 创建Vue应用实例
 const app = createApp(App)
 
-app.use(createPinia())
+// 使用Pinia状态管理
+app.use(pinia)
+
+// 使用路由
 app.use(router)
 
+// 挂载应用
 app.mount('#app')
